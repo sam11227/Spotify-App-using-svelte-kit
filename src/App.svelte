a665e8024d7b864174c3c63b9e49398f1c29e470
sam11227 <sambroung11@gmail.com>
2024-10-24 22:31:51 +0500

    update code

---
+++ src/App.svelte
@@ -0,0 +1,4 @@
+<script>
+    import Counter from "./lib/Counter.svelte";
+</script>
+<Counter/>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,24 @@
+<script>
+  let count = 0;
+  // let string = `you have clicled ${count} times`;
+
+  $: string = `you have clciked ${count} times`
+
+  
+  $: if (count > 5){
+    console.log(count, "update")
+  }
+
+  function increment(){
+    count += 1;
+  }
+</script>
+<button on:click={increment}>click {count}</button>
+<h3>{string}</h3>
+
+
+<style>
+  button{
+    padding: 15px 20px;
+  }
+</style>
